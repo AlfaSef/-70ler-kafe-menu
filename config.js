c148376d0f7e6be0bd63a495b3325eef9bdf01f8
AlfaSef <123488770+AlfaSef@users.noreply.github.com>
2026-09-03 21:10:07 +0300

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
   url: "https://ssccfrvbzlapnnqeruhy.supabase.co",
-  publishableKey: "BURAYA_SENDEKİ_MEVCUT_PUBLISHABLE_KEY"
+  publishableKey: "sb_publishable_srP9mm2mgcGv9Ac-ZZdpQg_WROSs6py"
 };
