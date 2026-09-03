939df5ce19735cb9686591109d7575dcea560cf5
AlfaSef <123488770+AlfaSef@users.noreply.github.com>
2026-09-03 21:01:29 +0300

    config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // Secret / service_role key'i kesinlikle buraya koymayın.
 
 window.SUPABASE_CONFIG = {
-  url: "https://ssccfrvbzlapnnqeruhy.supabase.co/rest/v1/",
+  url: "https://ssccfrvbzlapnnqeruh.supabase.co",
   publishableKey: "sb_publishable_srP9mm2mgcGv9Ac-ZZdpQg_WROSs6py"
 };
