8e1dfda9756572507280eaa9d2b6fb8775d66fda
AlfaSef <123488770+AlfaSef@users.noreply.github.com>
2026-09-18 13:51:33 +0300

    Create Android main activity

---
+++ android/app/src/main/java/com/seventies/cafe/personel/MainActivity.java
@@ -0,0 +1,29 @@
+package com.seventies.cafe.personel;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setSupportZoom(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://70lerkafe.com/personel.html");
+    }
+    @Override public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
